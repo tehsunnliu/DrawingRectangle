df5de629033870bb856ce232dc64482f80fb922c
Alex liu <alexliu_rocksu@yahoo.co.in>
2016-05-28 16:50:41 +0800

    Added zoom, need to add panning..

---
+++ DrawRectangle/ResizableRectangles/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace ResizableRectangles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButtonDrawRect = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButtonLoadImage = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonSaveImage = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonClearCanvas = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator6 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelZoom = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator4,
+            this.toolStripButtonDrawRect,
+            this.toolStripSeparator2,
+            this.toolStripButtonLoadImage,
+            this.toolStripButtonSaveImage,
+            this.toolStripSeparator5,
+            this.toolStripButtonClearCanvas,
+            this.toolStripSeparator3,
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripSeparator6});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(866, 32);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 32);
+            // 
+            // toolStripButtonDrawRect
+            // 
+            this.toolStripButtonDrawRect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonDrawRect.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripButtonDrawRect.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonDrawRect.Image")));
+            this.toolStripButtonDrawRect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonDrawRect.Name = "toolStripButtonDrawRect";
+            this.toolStripButtonDrawRect.Size = new System.Drawing.Size(27, 29);
+            this.toolStripButtonDrawRect.Text = "□";
+            this.toolStripButtonDrawRect.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.toolStripButtonDrawRect.Click += new System.EventHandler(this.toolStripButtonDrawRect_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 32);
+            // 
+            // toolStripButtonLoadImage
+            // 
+            this.toolStripButtonLoadImage.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonLoadImage.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonLoadImage.Image")));
+            this.toolStripButtonLoadImage.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonLoadImage.Name = "toolStripButtonLoadImage";
+            this.toolStripButtonLoadImage.Size = new System.Drawing.Size(73, 29);
+            this.toolStripButtonLoadImage.Text = "Load Image";
+            this.toolStripButtonLoadImage.Click += new System.EventHandler(this.toolStripButtonLoadImage_Click);
+            // 
+            // toolStripButtonSaveImage
+            // 
+            this.toolStripButtonSaveImage.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonSaveImage.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonSaveImage.Image")));
+            this.toolStripButtonSaveImage.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonSaveImage.Name = "toolStripButtonSaveImage";
+            this.toolStripButtonSaveImage.Size = new System.Drawing.Size(71, 29);
+            this.toolStripButtonSaveImage.Text = "Save Image";
+            this.toolStripButtonSaveImage.Click += new System.EventHandler(this.toolStripButtonSaveImage_Click);
+            // 
+            // toolStripButtonClearCanvas
+            // 
+            this.toolStripButtonClearCanvas.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonClearCanvas.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonClearCanvas.Image")));
+            this.toolStripButtonClearCanvas.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonClearCanvas.Name = "toolStripButtonClearCanvas";
+            this.toolStripButtonClearCanvas.Size = new System.Drawing.Size(79, 29);
+            this.toolStripButtonClearCanvas.Text = "Clear Canvas";
+            this.toolStripButtonClearCanvas.Click += new System.EventHandler(this.toolStripButtonClearCanvas_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 32);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(601, 423);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 29);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 29);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripSeparator6
+            // 
+            this.toolStripSeparator6.Name = "toolStripSeparator6";
+            this.toolStripSeparator6.Size = new System.Drawing.Size(6, 32);
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(6, 32);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelZoom,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 463);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(866, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelZoom
+            // 
+            this.toolStripStatusLabelZoom.AutoSize = false;
+            this.toolStripStatusLabelZoom.Name = "toolStripStatusLabelZoom";
+            this.toolStripStatusLabelZoom.Size = new System.Drawing.Size(65, 17);
+            this.toolStripStatusLabelZoom.Text = "Zoom : 0x";
+            this.toolStripStatusLabelZoom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 29);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(610, 426);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 485);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Drawing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonDrawRect;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton toolStripButtonLoadImage;
+        private System.Windows.Forms.ToolStripButton toolStripButtonClearCanvas;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton toolStripButtonSaveImage;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator6;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelZoom;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ DrawRectangle/ResizableRectangles/RectangleList.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ResizableRectangles
+{
+    class RectangleList
+    {
+        public Rectangle rectangle;
+        public List<Rectangle> subRectList;
+        private int m_nOffset = 5;
+
+        public RectangleList()
+        {
+        }
+
+        public RectangleList(int x, int y, int width, int height)
+        {
+            Size sz = new Size(m_nOffset, m_nOffset);
+            rectangle = new Rectangle(x, y, width, height);
+
+            subRectList = new List<Rectangle>(8);                                                                                                       // Set Resizable points for rectangle.
+            subRectList.Add(new Rectangle(new Point(rectangle.X - m_nOffset, rectangle.Y - m_nOffset), sz));                                            // Top Left
+            subRectList.Add(new Rectangle(new Point(rectangle.X + (rectangle.Width / 2) - (m_nOffset / 2), rectangle.Y - m_nOffset), sz));              // Top Center
+            subRectList.Add(new Rectangle(new Point(rectangle.X + rectangle.Width, rectangle.Y - m_nOffset), sz));                                      // Top Right
+
+            subRectList.Add(new Rectangle(new Point(rectangle.X - m_nOffset, rectangle.Y + (rectangle.Height / 2) - (m_nOffset / 2)), sz));             // Center Left
+            subRectList.Add(new Rectangle(new Point(rectangle.X + rectangle.Width, rectangle.Y + (rectangle.Height / 2) - (m_nOffset / 2)), sz));       // Center Right
+
+            subRectList.Add(new Rectangle(new Point(rectangle.X - m_nOffset, rectangle.Y + rectangle.Height), sz));                                     // Bottom Left
+            subRectList.Add(new Rectangle(new Point(rectangle.X + (rectangle.Width / 2) - (m_nOffset / 2), rectangle.Y + rectangle.Height), sz));       // Bottom Center
+            subRectList.Add(new Rectangle(new Point(rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height), sz));                           // Bottom Right
+        }
+
+        public void SetNewLocationOfSubRectList()
+        {
+            Size sz = new Size(m_nOffset, m_nOffset);
+            subRectList[0] = new Rectangle(new Point(rectangle.X - m_nOffset, rectangle.Y - m_nOffset), sz);
+            subRectList[1] = new Rectangle(new Point(rectangle.X + (rectangle.Width / 2) - (m_nOffset / 2), rectangle.Y - m_nOffset), sz);              // Top Center
+            subRectList[2] = new Rectangle(new Point(rectangle.X + rectangle.Width, rectangle.Y - m_nOffset), sz);                                      // Top Right
+
+            subRectList[3] = new Rectangle(new Point(rectangle.X - m_nOffset, rectangle.Y + (rectangle.Height / 2) - (m_nOffset / 2)), sz);             // Center Left
+            subRectList[4] = new Rectangle(new Point(rectangle.X + rectangle.Width, rectangle.Y + (rectangle.Height / 2) - (m_nOffset / 2)), sz);       // Center Right
+
+            subRectList[5] = new Rectangle(new Point(rectangle.X - m_nOffset, rectangle.Y + rectangle.Height), sz);                                     // Bottom Left
+            subRectList[6] = new Rectangle(new Point(rectangle.X + (rectangle.Width / 2) - (m_nOffset / 2), rectangle.Y + rectangle.Height), sz);       // Bottom Center
+            subRectList[7] = new Rectangle(new Point(rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height), sz);
+        }
+
+        public void ResizeRect()
+        {
+
+        }
+    }
+}
